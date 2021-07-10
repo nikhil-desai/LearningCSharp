ac3e4255e1b7b8b5ac0170cf778c93c2c5c6ed63
Nikhil Desai <61434199+nikhil-desai@users.noreply.github.com>
2021-07-10 14:47:13 -0400

    Starting Learning
    Hello World

---
+++ LearningCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearningCSharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
