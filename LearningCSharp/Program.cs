d20a252e91a54f564e18382152e0aa8f2853cb64
Nikhil Desai <61434199+nikhil-desai@users.noreply.github.com>
2021-07-12 20:36:50 -0400

    Lesson 1
    Console Read/Write

--- LearningCSharp/Program.cs
+++ LearningCSharp/Program.cs
@@ -6,7 +6,8 @@ namespace LearningCSharp
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press Enter to Continue!");
+            Console.ReadLine();
         }
     }
 }
